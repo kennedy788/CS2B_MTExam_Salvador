3a785650f0f0397460463684cb554920b0e12aa2
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-11-04 18:56:22 +0800

    Create userProfile.js

---
+++ userProfile.js
@@ -0,0 +1,10 @@
+let fullName = "John Kennedy A. Salvador"; 
+let age = 19; 
+let favoriteNumber = 13; 
+let favoriteColor = "Black"; 
+
+console.log("User Information:");
+console.log("Full Name: " + fullName);
+console.log("Age: " + age);
+console.log("Favorite Number: " + favoriteNumber);
+console.log("Favorite Color: " + favoriteColor);
