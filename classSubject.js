0687bb5614766965cd0a2254a2b856dd8c9e4d5e
kennedy788 <salvadorjohnkennedy78@gmail.com>
2024-11-04 19:19:24 +0800

    Create classSubject.js

---
+++ classSubject.js
@@ -0,0 +1,10 @@
+let subjectTitle = "Data Structures And Algorithms";
+let classSchedule = "Tuesday and Thursday 8:00am to 9:00am";
+let classroom = "Com Lab";
+let classInstructor = "Victruvius John Barayuga";
+let studentName = "John Kennedy A. Salvador";
+
+
+console.log(
+  `${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`
+);
